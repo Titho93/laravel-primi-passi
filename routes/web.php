aac5834d5b2d35cfcdd43f0fc1227e9a89c026a8
titho93 <titho93.10@gmail.com>
2023-11-16 14:54:17 +0100

    [add]file home.php and insert dinamic text

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $name = 'Tms';
+    $lastname = 'Brt';
+    return view('home', compact('name', 'lastname'));
 });
